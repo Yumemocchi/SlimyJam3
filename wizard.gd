c4d706f28ffdaf11e3ade3c3efd884105662495c
Yumepix <yumepix@pm.me>
2023-05-17 20:43:57 +0200

    correction deplacement

--- wizard.gd
+++ wizard.gd
@@ -1,13 +1,13 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-
+const SPEED = 10.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var _wizard_mesh = $Armature/Wizard
-
+@onready var _last_direction := Vector3.FORWARD
+@onready var _direction := Vector3.ZERO
 
 func  _ready() -> void:
 	#_animation.set("parameters/Blend3/blend_amount",-1)
@@ -21,13 +21,24 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() # * quaternion.from_euler(Vector3(0,0,0)
-	print(direction)
+		
+	input_dir.normalized()
+	_direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))# * quaternion.from_euler(Vector3(0,0,0)
+		
+	var rotation_speed = 5.0
+	if _direction.length() > 0.1:
+		var target_rotation = atan2(_direction.x, _direction.z)
+		_wizard_mesh.rotation.y = lerp_angle(_wizard_mesh.rotation.y, target_rotation, rotation_speed * delta)
+		_last_direction = Vector3(_direction.x,0.0,_direction.z)
+
+	print(_last_direction)
+	print(_direction)
+	
 	var is_attacking = Input.is_action_just_pressed("punch")
 
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if _direction:
+		velocity.x = _direction.x * SPEED
+		velocity.z = _direction.z * SPEED
 	#	_animation.set("parameters/Blend3/blend_amount",1)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -38,14 +49,7 @@ func _physics_process(delta):
 		print("attack")
 		attack()		
 
-	var rotation_speed = 5.0
-	var target_rotation = atan2(input_dir.x, input_dir.y)
-	_wizard_mesh.rotation.y = lerp_angle(_wizard_mesh.rotation.y, target_rotation, rotation_speed * delta)
-	#_wizard_mesh.rotation.y = atan2(direction.x,direction.z)
-
 	move_and_slide()
-	
-
 	
 func attack():
 	pass
